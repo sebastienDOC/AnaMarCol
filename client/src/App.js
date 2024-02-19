8b4f25d546ebe5785989911841156e383074e786
sebastienDOC <s.dechand21@gmail.com>
2024-02-19 14:14:01 +0100

    jwt

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ export default function App() {
     const fetchToken = async() => {
       await axios({
         method: 'get',
-        url: `${process.env.REACT_APP_API_URL}jwtid`,
+        url: `${process.env.REACT_APP_API_URL}/jwtid`,
         withCredentials: true
       })
       .then((res) => setUid(res.data))
